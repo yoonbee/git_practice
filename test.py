752303ebb4085e2450f9a4ce85cfe526a6a3694a
yoonbee <kimyoonbee7@gmail.com>
2022-10-27 01:29:13 +0900

    multiple

--- test.py
+++ test.py
@@ -4,7 +4,11 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
+def mul(a, b):
+    return a * b
+
 a = int(input())
 b = int(input())
 print(str(a) + '+' + str(b) + '=' + str(add(a, b)))
 print(str(a) + '-' + str(b) + '=' + str(sub(a, b)))
+print(str(a) + '*' + str(b) + '=' + str(mul(a, b)))
