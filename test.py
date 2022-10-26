68cbd978a87bb7d7de4886a13899e3edbf7f3e09
yoonbee <kimyoonbee7@gmail.com>
2022-10-27 01:21:19 +0900

    new file

---
+++ test.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+a = int(input())
+b = int(input())
+print(str(a) + '+' + str(b) + '=' + str(add(a, b)))
+print(str(a) + '-' + str(b) + '=' + str(sub(a, b)))
